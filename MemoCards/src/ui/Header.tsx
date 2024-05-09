52f004ee2608b38460c51260ecbb49fa06781d6e
RobicaCodeaza <stoicarobert38@yahoo.com>
2024-05-09 20:32:25 +0300

    Feature/Header - Completed

--- MemoCards/src/ui/Header.tsx
+++ MemoCards/src/ui/Header.tsx
@@ -5,7 +5,7 @@ import UserAvatar from '../features/authentication/UserAvatar'
 function Header() {
     return (
         <div
-            className=" flex items-center justify-end gap-[3.2rem] border-b border-mako-grey-100 bg-picton-blue-50 px-5 py-5 sm:relative 
+            className=" flex items-center justify-end  border-b border-mako-grey-100 bg-picton-blue-50 px-5 py-5 sm:relative 
             tab-port:px-10
             tab-land:px-12  
         particular-small-laptop:px-14
