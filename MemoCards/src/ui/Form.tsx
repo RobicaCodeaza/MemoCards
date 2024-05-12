2e83071d57fc0b3e389b4eab32cf845209c52506
Stoica Robert <stoicarobert38@yahoo.com>
2024-05-13 00:32:02 +0300

    Feature/Decks - Solved Form Error

--- MemoCards/src/ui/Form.tsx
+++ MemoCards/src/ui/Form.tsx
@@ -1,5 +1,5 @@
 import {
-    ElementType,
+    type ComponentPropsWithRef,
     type ComponentPropsWithoutRef,
     type ReactNode,
 } from 'react'
@@ -7,12 +7,12 @@ import {
 type FormProps = {
     children: ReactNode
     variation: 'regular' | 'modal'
-} & ComponentPropsWithoutRef<'form'>
+} & ComponentPropsWithRef<'form'>
 
 function Form({ children, variation, ...otherProps }: FormProps) {
     return (
         <form
-            className={`overflow-hidden text-[1.4rem] ${variation === 'regular' ? 'bg-picton-blue-50' : 'w-auto'} flex flex-col items-end gap-10 rounded-md border border-mako-grey-100 px-16 py-10`}
+            className={`overflow-hidden text-[1.4rem] ${variation === 'regular' ? 'bg-picton-blue-50' : 'w-auto'}  flex max-w-[45rem] flex-col  gap-10 rounded-md border border-mako-grey-100 px-16 py-10`}
             {...otherProps}
         >
             {children}
--- MemoCards/src/ui/FormRow.tsx
+++ MemoCards/src/ui/FormRow.tsx
@@ -19,7 +19,7 @@ function FormRow({ children, label, error }: FormRowProps) {
 
     return (
         <div
-            className={`flex flex-col justify-center gap-5 has-[button]:justify-end  has-[button]:gap-4 phone:items-center phone:gap-10 tab-port:flex-row`}
+            className={`flex flex-col  flex-wrap gap-5  has-[button]:justify-end has-[button]:gap-4  phone:items-center phone:gap-4 tab-port:flex-row`}
         >
             {React.Children.map(children, (child) => {
                 if (isReactElementWithId(child)) {
@@ -27,7 +27,7 @@ function FormRow({ children, label, error }: FormRowProps) {
                         <>
                             {label && (
                                 <label
-                                    className="text-[1.4rem] uppercase tracking-wide text-mako-grey-700"
+                                    className="ml-auto mr-auto text-[1.4rem] uppercase tracking-wide text-mako-grey-700"
                                     htmlFor={child.props.id}
                                 >
                                     {label}
@@ -35,7 +35,7 @@ function FormRow({ children, label, error }: FormRowProps) {
                             )}
                             {child}
                             {error && (
-                                <span className="text-danger-500">
+                                <span className="ml-auto text-danger-500">
                                     {error as ReactNode}
                                 </span>
                             )}
@@ -46,7 +46,5 @@ function FormRow({ children, label, error }: FormRowProps) {
         </div>
     )
 }
-
-FormRow.defaultProps = { direction: 'row' }
 
 export default FormRow
