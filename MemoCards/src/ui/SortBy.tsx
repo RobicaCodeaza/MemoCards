e970ef0a5c362f4817d0d95038d619583c17d64e
Stoica Robert <stoicarobert38@yahoo.com>
2024-05-18 15:45:30 +0300

    Feature/Ui - Finished SortBy Functionality and Ui

--- MemoCards/src/ui/SortBy.tsx
+++ MemoCards/src/ui/SortBy.tsx
@@ -1,5 +1,29 @@
-function SortBy() {
-    return <div></div>
+import { useSearchParams } from 'react-router-dom'
+import Select from './Select'
+import { ChangeEvent } from 'react'
+
+export type OptionsType = { value: string; label: string }[]
+
+type SortByProps = {
+    options: OptionsType
+}
+
+function SortBy({ options }: SortByProps) {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const currentSortBy = searchParams.get('sortBy') ?? ''
+
+    function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+        searchParams.set('sortBy', e.target.value)
+        setSearchParams(searchParams)
+    }
+
+    return (
+        <Select
+            onChange={handleChange}
+            options={options}
+            value={currentSortBy}
+        ></Select>
+    )
 }
 
 export default SortBy
