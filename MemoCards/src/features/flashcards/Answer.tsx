503b9ce7a882ab64d5be8d5f748cea53b8f7c956
Stoica Robert <stoicarobert38@yahoo.com>
2024-05-23 13:52:29 +0300

    Feature/Cards - Modified Colors

--- MemoCards/src/features/flashcards/Answer.tsx
+++ MemoCards/src/features/flashcards/Answer.tsx
@@ -9,13 +9,13 @@ type AnswerProps = {
 function Answer({ children, correctAnswer, numberAnswer }: AnswerProps) {
     if (correctAnswer === numberAnswer + 1)
         return (
-            <div className="rounded bg-chateau-green-200 px-6 py-2 text-[1.5rem] tracking-wide text-chateau-green-800">
+            <div className="rounded-md border border-[transparent] bg-chateau-green-200  px-6 py-2 text-[1.5rem] tracking-wide text-chateau-green-800">
                 {children}
             </div>
         )
     else
         return (
-            <div className="rounded bg-danger-200 px-6 py-2 text-[1.5rem] tracking-wide text-danger-800">
+            <div className="bg-picton-blue rounded-md  bg-picton-blue-50 px-6 py-2 text-[1.5rem]  tracking-wide text-danger-700">
                 {children}
             </div>
         )
--- MemoCards/src/features/flashcards/NumberAnswer.tsx
+++ MemoCards/src/features/flashcards/NumberAnswer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 function NumberAnswer({ children }: PropsWithChildren) {
     return (
-        <div className="rounded  bg-picton-blue-400 px-2 py-2 text-center text-picton-blue-50">
+        <div className="rounded border border-picton-blue-500  bg-picton-blue-50 px-2 py-2 text-center text-picton-blue-500">
             A{children}
         </div>
     )
