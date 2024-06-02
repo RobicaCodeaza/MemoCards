cdfec31b477424a4cf0ab489ab2c0fff53875e52
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-02 18:16:24 +0300

    Feature/Quiz - Added Animation on loading

--- MemoCards/src/features/quiz/QuizCard.tsx
+++ MemoCards/src/features/quiz/QuizCard.tsx
@@ -20,16 +20,23 @@ function QuizCard({ quiz }: QuizCardProps) {
     const { deleteQuiz, isDeleting } = useDeleteQuiz()
 
     return (
-        <div className=" flex h-[25rem] w-full flex-col gap-6  rounded-lg border border-chateau-green-200   px-6 py-6 shadow-lg   phone:px-8 phone:py-8 tab-port:px-8 tab-port:py-8 tab-land:px-10  tab-land:py-10 particular-small-laptop:px-12 particular-small-laptop:py-12">
-            <div className="flex items-start justify-between">
-                <div className="flex flex-col ">
-                    <p className="text-[1.5rem] font-medium uppercase">
-                        {capitalizeHeader(quiz.quizName)}
-                    </p>
-                    <p className="text-[1.4rem] text-mako-grey-500">
-                        {quiz.description}
-                    </p>
-                    {/* <p className="text-[1.4rem] text-mako-grey-500">
+        <motion.div
+            initial={{ opacity: 0, scale: 0.8, y: 50 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.2, y: -50 }}
+            transition={{ duration: 0.3 }}
+            key={quiz.id}
+        >
+            <div className=" flex h-[25rem] w-full flex-col gap-6  rounded-lg border border-chateau-green-200   px-6 py-6 shadow-lg   phone:px-8 phone:py-8 tab-port:px-8 tab-port:py-8 tab-land:px-10  tab-land:py-10 particular-small-laptop:px-12 particular-small-laptop:py-12">
+                <div className="flex items-start justify-between">
+                    <div className="flex flex-col ">
+                        <p className="text-[1.5rem] font-medium uppercase">
+                            {capitalizeHeader(quiz.quizName)}
+                        </p>
+                        <p className="text-[1.4rem] text-mako-grey-500">
+                            {quiz.description}
+                        </p>
+                        {/* <p className="text-[1.4rem] text-mako-grey-500">
                         Time per Question
                     </p>
                     <p className="text-[1.4rem] text-mako-grey-500">
@@ -38,71 +45,72 @@ function QuizCard({ quiz }: QuizCardProps) {
                     <p className="text-[1.4rem] text-mako-grey-500">
                         Solved Time
                     </p> */}
+                    </div>
+                    <Modal>
+                        <Menus.Menu>
+                            <Menus.Toggle id={quiz.id}></Menus.Toggle>
+                            <Menus.List id={quiz.id}>
+                                <Modal.Open opens="editQuiz">
+                                    <Menus.Button
+                                        icon={
+                                            <CiEdit className="h-8 w-8 text-picton-blue-600"></CiEdit>
+                                        }
+                                    >
+                                        Edit Quiz
+                                    </Menus.Button>
+                                </Modal.Open>
+
+                                <Modal.Open opens="deleteQuiz">
+                                    <Menus.Button
+                                        icon={
+                                            <RiDeleteBin7Line className="h-7 w-8 text-danger-500"></RiDeleteBin7Line>
+                                        }
+                                    >
+                                        Delete Quiz
+                                    </Menus.Button>
+                                </Modal.Open>
+                            </Menus.List>
+                        </Menus.Menu>
+                        <Modal.Window name="deleteQuiz">
+                            {isDeleting ? (
+                                <Spinner></Spinner>
+                            ) : (
+                                <ConfirmDelete
+                                    resourceName="Quiz"
+                                    disabled={isDeleting}
+                                    onConfirm={() => deleteQuiz(quiz.id)}
+                                ></ConfirmDelete>
+                            )}
+                        </Modal.Window>
+                        <Modal.Window type="quiz" name="editQuiz">
+                            <CreateQuizForm quizToEdit={quiz}></CreateQuizForm>
+                        </Modal.Window>
+                    </Modal>
                 </div>
-                <Modal>
-                    <Menus.Menu>
-                        <Menus.Toggle id={quiz.id}></Menus.Toggle>
-                        <Menus.List id={quiz.id}>
-                            <Modal.Open opens="editQuiz">
-                                <Menus.Button
-                                    icon={
-                                        <CiEdit className="h-8 w-8 text-picton-blue-600"></CiEdit>
-                                    }
-                                >
-                                    Edit Quiz
-                                </Menus.Button>
-                            </Modal.Open>
 
-                            <Modal.Open opens="deleteQuiz">
-                                <Menus.Button
-                                    icon={
-                                        <RiDeleteBin7Line className="h-7 w-8 text-danger-500"></RiDeleteBin7Line>
-                                    }
-                                >
-                                    Delete Quiz
-                                </Menus.Button>
-                            </Modal.Open>
-                        </Menus.List>
-                    </Menus.Menu>
-                    <Modal.Window name="deleteQuiz">
-                        {isDeleting ? (
-                            <Spinner></Spinner>
-                        ) : (
-                            <ConfirmDelete
-                                resourceName="Quiz"
-                                disabled={isDeleting}
-                                onConfirm={() => deleteQuiz(quiz.id)}
-                            ></ConfirmDelete>
-                        )}
-                    </Modal.Window>
-                    <Modal.Window type="quiz" name="editQuiz">
-                        <CreateQuizForm quizToEdit={quiz}></CreateQuizForm>
-                    </Modal.Window>
-                </Modal>
-            </div>
-
-            <div className=" mb-auto mt-auto flex  flex-col gap-6 text-[1.35rem] uppercase tracking-wide">
-                <label
-                    htmlFor="progress"
-                    className="text-center text-neon-carrot-900"
-                >
-                    Perfection Score:{' '}
-                    <strong>
-                        {quiz.perfectionScore ? quiz.perfectionScore : 0}
-                    </strong>
-                </label>
-                <div className="">
-                    <Progress
-                        value={quiz.perfectionScore}
-                        className="mx-16 w-[auto] border border-neon-carrot-500 bg-neon-carrot-300  "
-                    />
+                <div className=" mb-auto mt-auto flex  flex-col gap-6 text-[1.35rem] uppercase tracking-wide">
+                    <label
+                        htmlFor="progress"
+                        className="text-center text-neon-carrot-900"
+                    >
+                        Perfection Score:{' '}
+                        <strong>
+                            {quiz.perfectionScore ? quiz.perfectionScore : 0}
+                        </strong>
+                    </label>
+                    <div className="">
+                        <Progress
+                            value={quiz.perfectionScore}
+                            className="mx-16 w-[auto] border border-neon-carrot-500 bg-neon-carrot-300  "
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <Button variation="accentTertiary" size="small">
-                Take Test
-            </Button>
-        </div>
+                <Button variation="accentTertiary" size="small">
+                    Take Test
+                </Button>
+            </div>
+        </motion.div>
     )
 }
 
