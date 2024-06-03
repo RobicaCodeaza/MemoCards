827e11e7938f2a54bfc10b14124203345b6c67e8
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-03 23:08:39 +0300

    Feature/Decks - Added Page Reset on Deletion

--- MemoCards/src/features/decks/useDeleteAllDecks.ts
+++ MemoCards/src/features/decks/useDeleteAllDecks.ts
@@ -3,6 +3,7 @@ import { deleteAllDecks as deleteAllDecksApi } from '@/services/apiDecks'
 import { UserType } from '@/ui/ProtectedRoute'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import toast from 'react-hot-toast'
+import { useSearchParams } from 'react-router-dom'
 
 export function useDeleteAllDecks() {
     const [user, _] = useLocalStorageState<UserType>(
@@ -14,10 +15,14 @@ export function useDeleteAllDecks() {
     )
     const queryClient = useQueryClient()
 
+    const [searchParams, setSearchParams] = useSearchParams()
+
     const { isLoading: isDeletingAll, mutate: deleteAllDecks } = useMutation({
         mutationFn: () => deleteAllDecksApi(user.user_id),
         onSuccess: async () => {
             toast.success('All Decks successfully deleted.')
+            searchParams.delete('page')
+            setSearchParams(searchParams)
             await queryClient.invalidateQueries({
                 queryKey: ['decks', 'cards'],
             })
