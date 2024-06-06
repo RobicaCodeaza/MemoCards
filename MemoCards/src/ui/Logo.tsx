0f92825b4d39a10538231087cdc464a8ed90dc3b
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-06 14:31:51 +0300

    Feature/Ui - Modified Logo Size

--- MemoCards/src/ui/Logo.tsx
+++ MemoCards/src/ui/Logo.tsx
@@ -2,7 +2,7 @@ import logo from '../../public/logo.png'
 
 function Logo() {
     return (
-        <div className="flex h-20 justify-center particular-small-laptop:h-24">
+        <div className="flex h-12 justify-center particular-small-laptop:h-16">
             <img className="object-contain" alt="Logo" src={logo}></img>
         </div>
     )
