cbbe4065eac28f6392abed2ec8bc0b84ac0f5262
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-06 21:13:52 +0300

    Feature/Ui - Restyled Sidebar Gap

--- MemoCards/src/ui/Sidebar.tsx
+++ MemoCards/src/ui/Sidebar.tsx
@@ -7,7 +7,7 @@ function Sidebar() {
 
     return (
         <div
-            className={`absolute row-span-full flex h-screen w-[20rem] flex-col  gap-[3.2rem] border-r border-mako-grey-100 bg-picton-blue-50 px-6 py-6 transition-[all] duration-300 ease-in-out tab-port:relative tab-port:w-auto tab-port:translate-x-0 tab-port:px-3 tab-port:py-6 ${openMenu ? 'z-10 translate-x-0' : 'translate-x-[-100%]'}`}
+            className={`absolute row-span-full flex h-screen w-[20rem] flex-col  gap-[2.5rem] border-r border-mako-grey-100 bg-picton-blue-50 px-6 py-6 transition-[all] duration-300 ease-in-out tab-port:relative tab-port:w-auto tab-port:translate-x-0 tab-port:px-3 tab-port:py-6 ${openMenu ? 'z-10 translate-x-0' : 'translate-x-[-100%]'}`}
         >
             <Logo></Logo>
             <MainNav></MainNav>
