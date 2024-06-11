2c18bfb4972f3f1149bc2fcc3ce7df521e662f3f
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-11 20:15:14 +0300

    Feature/Quiz - Updated StartScreen of Testing

--- MemoCards/src/features/quiz/Testing/StartScreen.tsx
+++ MemoCards/src/features/quiz/Testing/StartScreen.tsx
@@ -1,17 +1,21 @@
 import Heading from '@/ui/Heading'
-import { start } from '../quizSlice'
+import { getQuizNumQuestions, start } from '../quizSlice'
 import Button from '@/ui/Button'
 import { useAppDispatch } from '@/hooks/useAppDispatch'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { useAppSelector } from '@/hooks/useAppSelector'
 
 function StartScreen() {
     const dispatch = useAppDispatch()
+    const numQuestions = useAppSelector(getQuizNumQuestions)
 
     return (
         <div className="flex flex-col items-center gap-14 p-12">
             <div className="flex flex-col items-center gap-2">
                 <Heading as="h2">Welcome to your quiz.</Heading>
-                <Heading as="h3">3 questions to master your lessons.</Heading>
+                <Heading as="h3">
+                    {numQuestions} questions to master your lessons.
+                </Heading>
             </div>
             <DotLottieReact
                 autoplay
