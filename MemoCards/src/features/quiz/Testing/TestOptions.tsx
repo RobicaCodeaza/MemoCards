462af0858011ceb292c4b5cfa751b5f8dfdeae14
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-18 20:01:55 +0300

    Feature/Quiz - Responsive Quiz Part 3

--- MemoCards/src/features/quiz/Testing/TestOptions.tsx
+++ MemoCards/src/features/quiz/Testing/TestOptions.tsx
@@ -84,7 +84,7 @@ function TestOptions({ indexQuestion, ...props }: TestOptionsProps) {
                         ) : (
                             <button
                                 key={index}
-                                className={`block w-full cursor-pointer text-wrap break-words  rounded-full border-2 px-10 py-5 text-left font-sans text-[1.6rem] transition-all duration-300 hover:translate-x-5 hover:border-picton-blue-300 hover:bg-picton-blue-150 disabled:hover:cursor-not-allowed ${index + 1 === answerQuiz ? 'translate-x-10' : ''} ${hasAnswered || hasFinishedQuestionTime ? (index + 1 === question.correctAnswer ? 'border border-chateau-green-300 bg-chateau-green-200 text-chateau-green-700' : 'border-danger-300 bg-danger-200 text-danger-700') : 'border-picton-blue-150 bg-picton-blue-75'} `}
+                                className={`block w-full cursor-pointer text-wrap break-words  rounded-full border-2 px-10 py-5 text-left font-sans text-[1.4rem] transition-all duration-300 hover:translate-x-5 hover:border-picton-blue-300 hover:bg-picton-blue-150 disabled:hover:cursor-not-allowed phone:text-[1.6rem] ${index + 1 === answerQuiz ? 'translate-x-10' : ''} ${hasAnswered || hasFinishedQuestionTime ? (index + 1 === question.correctAnswer ? 'border border-chateau-green-300 bg-chateau-green-200 text-chateau-green-700' : 'border-danger-300 bg-danger-200 text-danger-700') : 'border-picton-blue-150 bg-picton-blue-75'} `}
                                 disabled={hasFinishedQuestionTime}
                                 onClick={() =>
                                     dispatch(
