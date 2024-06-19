738c9aa07f9a1295551059f6c2ad711d84ffb70f
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-19 21:10:12 +0300

    Feature/Cards - Some Responsive Changes

--- MemoCards/src/features/flashcards/CorrectAnswer.tsx
+++ MemoCards/src/features/flashcards/CorrectAnswer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 function CorrectAnswer({ children }: PropsWithChildren) {
     return (
-        <div className="w-1/2 justify-self-center rounded bg-chateau-green-500 px-2 py-2 text-center font-medium text-chateau-green-50">
+        <div className=" justify-self-center rounded bg-chateau-green-500 px-2 py-2 text-center font-medium text-chateau-green-50 phone:w-1/2">
             A{children}
         </div>
     )
