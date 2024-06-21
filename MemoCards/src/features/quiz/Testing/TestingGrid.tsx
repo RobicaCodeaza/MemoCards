c5c2b15a8ae9873f82d6a3b6461c95b3fa89861d
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-22 01:01:26 +0300

    Feature/Quiz - Some Responsive Changes

--- MemoCards/src/features/quiz/Testing/TestingGrid.tsx
+++ MemoCards/src/features/quiz/Testing/TestingGrid.tsx
@@ -27,7 +27,7 @@ function TestingGrid() {
         return <Empty resource="Decks/Cards for the coresponding Quiz"></Empty>
 
     return (
-        <main className="ml-auto  mr-auto flex h-[70rem]   w-full max-w-[120rem] flex-col gap-8 rounded-xl border-2 border-mako-grey-100 bg-picton-blue-50 px-4 py-10 phone:h-[65rem] phone:w-[65rem] tab-port:h-[65rem] tab-port:w-[75rem]   tab-land:h-[65rem] tab-land:w-[90rem] particular-small-laptop:h-[72.5rem] particular-small-laptop:w-[100rem]">
+        <main className="ml-auto  mr-auto flex h-[70rem]   w-full max-w-[120rem] flex-col gap-8 rounded-xl border-2 border-mako-grey-100 bg-picton-blue-50 px-4 py-10 phone:h-[65rem] phone:w-[65rem] phone:px-8 tab-port:h-[65rem]  tab-port:w-[75rem] tab-port:px-10 tab-port:py-12 tab-land:h-[65rem]   tab-land:w-[90rem] tab-land:px-12 particular-small-laptop:h-[72.5rem] particular-small-laptop:w-[100rem]">
             {/* {status === 'error' && <Error></Error>} */}
             {status === 'ready' && <StartScreen></StartScreen>}
             {status === 'active' && (
