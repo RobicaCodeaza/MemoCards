15ad2cd9c9bf544bf2f19eadac881acdd068ea55
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-28 13:39:15 +0300

    Feature/Dashboard - Structured Recap Plan

--- MemoCards/src/features/dashboard/RecapPlan.tsx
+++ MemoCards/src/features/dashboard/RecapPlan.tsx
@@ -1,19 +1,23 @@
 import RecapPlanCard from './RecapPlan/RecapPlanCard'
+import Heading from '@/ui/Heading'
 
 function RecapPlan() {
     return (
-        <div className="col-span-full flex gap-10 overflow-x-hidden rounded-lg border border-mako-grey-200 bg-picton-blue-75 px-4 py-4 phone:px-6 phone:py-6 tab-port:px-8 tab-port:py-8 tab-land:px-10 tab-land:py-10 particular-small-laptop:px-12 particular-small-laptop:py-12">
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
-            <RecapPlanCard></RecapPlanCard>
+        <div className="col-span-full flex flex-col gap-7">
+            <Heading as="h4">Recap Plan</Heading>
+            <div className="flex gap-12 overflow-x-scroll rounded-lg border border-mako-grey-150 bg-picton-blue-75 px-4 py-4 phone:px-6 phone:py-6 tab-port:px-8 tab-port:py-8 tab-land:px-10 tab-land:py-10 particular-small-laptop:px-12 particular-small-laptop:py-12">
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+                <RecapPlanCard></RecapPlanCard>
+            </div>
         </div>
     )
 }
--- MemoCards/src/features/dashboard/RecapPlan/RecapPlanCard.tsx
+++ MemoCards/src/features/dashboard/RecapPlan/RecapPlanCard.tsx
@@ -1,7 +1,18 @@
 function RecapPlanCard() {
     return (
-        <div className="w-[25rem] flex-grow rounded-md border-mako-grey-100 bg-chateau-green-400 px-4 py-12">
-            Some data
+        <div
+            className="shadow-custom-inset flex  h-[12.5rem] w-[12.5rem]  flex-shrink-0  flex-col justify-center gap-3 rounded-md  px-6 py-6 text-mako-grey-500"
+            // style={{ background: 'linear-gradient(135deg,#fb6e77,#fea3a9)' }}
+        >
+            <p className="text-[1.3rem] font-semibold tracking-wide">
+                Quiz Name
+            </p>
+            <p className="text-[1.3rem] font-medium tracking-wide">
+                ⭐: <strong>75</strong>p
+            </p>
+            <p className="text-[1.3rem] font-medium tracking-wide">
+                📆: in <strong>7</strong> days
+            </p>
         </div>
     )
 }
