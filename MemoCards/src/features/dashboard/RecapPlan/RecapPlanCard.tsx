d2af3a75ee6a966a3e64207f9acf0c1e5f2337f3
Stoica Robert <stoicarobert38@yahoo.com>
2024-06-29 18:03:23 +0300

    Feature/Dashboard - Added Redirect to Test from RecapPlanCard

--- MemoCards/src/features/dashboard/RecapPlan/RecapPlanCard.tsx
+++ MemoCards/src/features/dashboard/RecapPlan/RecapPlanCard.tsx
@@ -1,17 +1,21 @@
 import Button from '@/ui/Button'
 import { formatDistanceFromNow, fromToday } from '@/utils/helpers'
+import { useNavigate } from 'react-router-dom'
 
 type RecapPlanCardProps = {
     quizName: string
     perfectionScore: number
     toBeTested: string | null
+    quizId: number
 }
 
 function RecapPlanCard({
     quizName,
     perfectionScore,
     toBeTested,
+    quizId,
 }: RecapPlanCardProps) {
+    const navigate = useNavigate()
     return (
         <div
             className=" relative flex h-[12rem]  w-[14rem] flex-shrink-0  flex-col justify-center gap-3 rounded-md   border  border-danger-300 bg-picton-blue-50 px-2 py-2 text-mako-grey-500 shadow-glowing-border-p50 transition-shadow duration-300 hover:shadow-inner phone:px-4 phone:py-4 tab-port:px-6 tab-port:py-6"
@@ -32,7 +36,11 @@ function RecapPlanCard({
                 </strong>{' '}
             </p>
             <div className="absolute inset-0 flex items-center justify-center  rounded-md bg-picton-blue-50 opacity-0 shadow-inner transition-opacity duration-300 hover:opacity-100">
-                <Button size="tiny" variation="simpleTertiary">
+                <Button
+                    size="tiny"
+                    variation="simpleTertiary"
+                    onClick={() => navigate(`/quiz/${quizId}`)}
+                >
                     Take Quiz
                 </Button>
             </div>
