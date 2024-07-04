2171da0ff2f3bdf1b81d4dc664aae6151cff99af
RobicaCodeaza <stoicarobert38@yahoo.com>
2024-07-04 19:57:23 +0300

    Feature/Auth -- Implementing Login Demo

--- MemoCards/src/pages/Authentication.tsx
+++ MemoCards/src/pages/Authentication.tsx
@@ -1,7 +1,11 @@
 import Logo from '@/ui/Logo'
 import Login from '@/features/authentication/Login'
+import Button from '@/ui/Button'
+import { useLoginDemoAcc } from '@/features/authentication/useLoginDemoAcc'
 
 function Authentication() {
+    const { loginDemoAcc } = useLoginDemoAcc()
+
     return (
         <div className="flex min-h-screen items-center justify-center bg-picton-blue-100 py-6">
             <div className="w-[87.5%] rounded-[12px] border-2 border-mako-grey-200 bg-picton-blue-50 p-6 shadow-md phone:w-1/2 phone:p-10 tab-port:w-2/5 tab-port:p-12 tab-land:w-1/3 tab-land:p-16 particular-small-laptop:w-[30%] particular-small-laptop:p-24">
@@ -9,6 +13,13 @@ function Authentication() {
                 <p className="mb-10 mt-10 text-center">
                     Take a look at the next flashcards app
                 </p>
+                <Button
+                    variation="accentPrimary"
+                    size="medium"
+                    onClick={() => loginDemoAcc()}
+                >
+                    Login With Demo Account
+                </Button>
                 <Login></Login>
             </div>
         </div>
